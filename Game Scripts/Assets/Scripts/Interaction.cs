63e8db8976d4396d2e05edb97a973a0a0809c554
Kamarin N <109175221+disabop@users.noreply.github.com>
2022-07-12 12:26:28 -0700

    Initial Project Upload

---
+++ Game Scripts/Assets/Scripts/Interaction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private PlayerController playerControllerScript;
+    public GameObject player;
+    public Vector3 offset = new Vector3(4, 5, 0);
+    playerControllerScript = GameObject.Find("Player").GetComponent<Collidable();
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.position = player.transform.position + offset;
+    }
+}
