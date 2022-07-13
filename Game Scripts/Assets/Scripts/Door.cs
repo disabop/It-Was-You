45494680985249c3ff0a0584586c8d029ebde5c9
Kamarin N <109175221+disabop@users.noreply.github.com>
2022-07-13 10:38:15 -0700

    Script Updates

---
+++ Game Scripts/Assets/Scripts/Door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : Collidable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public string[] sceneNames;
+
+
+    protected override void OnCollide(Collider2D coll)
+    {
+        if (coll.name == "Player")
+        {
+            string sceneName = sceneNames[Random.Range(0, sceneNames.Length)];
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
