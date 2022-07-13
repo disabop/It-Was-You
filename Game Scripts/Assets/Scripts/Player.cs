fc668c6fe97df240b39b50954be201399a6703ea
Kamarin N <109175221+disabop@users.noreply.github.com>
2022-07-13 14:21:23 -0700

    Revert "sprinting"
    This reverts commit 42655151a7e4e0c3ca5092114ded901590be0fa1.

--- Game Scripts/Assets/Scripts/Player.cs
+++ Game Scripts/Assets/Scripts/Player.cs
@@ -8,8 +8,6 @@ public class Player : MonoBehaviour
 
     public Vector2 velocity;
 
-    private float shiftSpeed = 15.0f;
-
 
     Rigidbody2D myRB;
 
@@ -29,15 +27,6 @@ public class Player : MonoBehaviour
         velocity.y = Input.GetAxisRaw("Vertical") * speed;
 
         myRB.velocity = velocity;
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            speed = shiftSpeed;
-        }
-        else if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            speed = 10;
-        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
