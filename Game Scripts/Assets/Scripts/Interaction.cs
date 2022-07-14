687553a457ab4d46b1c4890fec8b8fa0c826b2f5
Kamarin N <109175221+disabop@users.noreply.github.com>
2022-07-14 13:30:26 -0700

    Newspaper Update

--- Game Scripts/Assets/Scripts/Interaction.cs
+++ Game Scripts/Assets/Scripts/Interaction.cs
@@ -9,6 +9,7 @@ public class Interaction : MonoBehaviour
     public KeyCode interactKey;
     public UnityEvent interactAction;
     public GameObject drawerText;
+    public GameObject newspaperText;
 
     private IEnumerator DrawerMessageDisappears()
     {
@@ -20,11 +21,22 @@ public class Interaction : MonoBehaviour
 
     }
 
+    private IEnumerator NewspaperMessageDisappears()
+    {
+        newspaperText.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(4);
+
+        newspaperText.gameObject.SetActive(false);
+
+    }
+
     private void Update()
     {
         if (isInRange)
         {
             drawerText.gameObject.SetActive(true);
+            newspaperText.gameObject.SetActive(true);
             if (Input.GetKeyDown(interactKey))
             {
                 interactAction.Invoke();
@@ -48,6 +60,7 @@ public class Interaction : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             drawerText.gameObject.SetActive(false);
+            newspaperText.gameObject.SetActive(false);
             isInRange = false;
             Debug.Log("Player is no longer in range");
         }
